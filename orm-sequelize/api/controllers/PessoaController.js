e10eeb8226adad2d3a72a4ee8bb87e9ce07fc6c2
Vinicius Guerra <102999190+Vinicius-Guerra@users.noreply.github.com>
2023-04-04 17:09:02 -0300

    Atualizar e Deletar - CRUD

--- orm-sequelize/api/controllers/PessoaController.js
+++ orm-sequelize/api/controllers/PessoaController.js
@@ -20,6 +20,41 @@ class PessoaController {
             return res.status(500).json({error: message});
         }
     }
+
+    static async criaPessoa (req, res) {
+        const novaPessoa = req.body;
+        
+        try {
+            const  novaPessoaCriada = await database.Pessoas.create(novaPessoa);
+            return res.status(200).json(novaPessoaCriada)
+        } catch (error) {
+            return res.status(500).json({error: message});
+        }
+    }
+
+    static async atualizaPessoa (req, res) {
+        const { id } = req.params;
+        const novasInfos = req.body;
+       
+        try {
+            await database.Pessoas.update(novasInfos, { where: { id: Number(id)}}
+            );
+            const pessoaAtualizada = await database.Pessoas.findOne({ where: { id: Number(id)}});
+            res.status(200).json(pessoaAtualizada);
+        } catch (error) {
+            return res.status(500).json({error: message});
+        }
+    }
+
+    static async apagaPessoa (req, res) {
+        const { id } = req.params;
+        try {
+            await database.Pessoas.destroy({ where: { id: Number(id)}});
+            res.status(200).json({ mensagem:  `O id : ${id} foi apagado com sucesso.`})
+        } catch (erroe) {
+            return res.status(500).json({error: message});
+        }
+    }
 }
 
 module.exports = PessoaController;
