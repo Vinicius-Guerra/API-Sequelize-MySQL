ac463d91f8ad99d7c6a6a96438fcefb9e7dcf8c2
Vinicius Guerra <102999190+Vinicius-Guerra@users.noreply.github.com>
2023-04-05 16:25:50 -0300

    att

--- orm-sequelize/api/models/pessoas.js
+++ orm-sequelize/api/models/pessoas.js
@@ -9,8 +9,13 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate(models) { //Associando com outras tabelas
+      Pessoas.hasMany(models.Turmas, {
+        foreignKey: 'docente_id'
+      });
+      Pessoas.hasMany(models.Matriculas, {
+        foreignKey: 'estudante_id'
+      });
     }
   }
   Pessoas.init({
